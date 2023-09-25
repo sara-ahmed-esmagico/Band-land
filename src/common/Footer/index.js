31fe151a9c9a4b2866a651f3a7d305aaad47ec41
Sara Ahmed <sara.ahmed1447@gmail.com>
2023-09-25 17:59:28 +0530

    footer image

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -1,98 +1,70 @@
-import React from "react";
+import React from 'react'
 import Stars from "../../images/Home/Stars.png";
 import Foot from "../../images/Home/Foot.png";
 import { Bolt } from "@mui/icons-material";
 import WhiteLogo from "../../images/Bandland Logo White.svg";
 import BMSLiveSvg from "../../images/BMS LIVE.svg";
 import "../../styles/Home.css";
-import { Box, Button, Typography } from "@mui/material";
-import FooterBase from "../../images/Home/SmallFooter.png";
-import { useTheme } from "@mui/material/styles";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import SmallFooter from "../../images/Home/SmallFooter.png";
+import { Box, Button, Typography } from '@mui/material';
+import FooterBase from "../../images/Home/Footer.png";
+
 const Footer = () => {
-  const theme = useTheme();
-  const isMediumScreen = useMediaQuery(theme.breakpoints.up("md"));
-  const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"));
-  const isScreenXtraSmall = useMediaQuery(theme.breakpoints.up("xs"));
-  console.log(isScreenXtraSmall, "lol");
   return (
-    <div className="pos-relative flex-center flex-col align-center">
+    <div className='pos-relative flex-center flex-col align-center'>
       <img
-        className="pos-absolute bottom-0 z-index-1"
-        src={Foot}
-        style={{ width: "100%" }}
-      />
-      <img
-        className="pos-absolute bottom-2 z-index-1"
-        src={Stars}
-        style={{ width: "100%" }}
-      />
-      <Box className="pos-relative z-index-4">
-        <img src={SmallFooter} className="small-footer  " />
-        <img
-          src={FooterBase}
-          className="large-footer"
+          className="pos-absolute bottom-0 z-index-1"
+          src={Foot}
+          style={{ width: "100%" }}
         />
-        {/* {isSmallScreen ? (
-          <>
-            
-          </>
-        ) : (
-         <>
-           </>
-        )}         */}
-
-        <Box
-          className="pos-absolute bottom-0 z-index-4 flex flex-col align-center"
-          width={"100%"}
-        >
-          <img src={WhiteLogo} className="rotate" width={"40%"} />
-
+        <img
+          className="pos-absolute bottom-2 z-index-1"
+          src={Stars}
+          style={{ width: "100%" }}
+        />
+        <Box className="pos-relative z-index-4">
+          <img src={FooterBase} className="footer" width={'100%'} />
           <Box
-            sx={{ flexDirection: { xs: "column", sm: "row" } }}
-            className="flex align-center"
+            className="pos-absolute  z-index-4 flex flex-col align-center"
+            width={"100%"}
+            sx={{ top: "30%" }}
           >
-            <Button
-              href="/"
-              className="font-modesto-condensed btn-text"
-              sx={{
-                px: 0,
-                fontSize: { xs: "20px", sm: "40px" },
-                mt: { xs: -1, sm: 0 },
-              }}
-            >
-              HOME
-            </Button>
-            <Bolt
-              sx={{ px: 0, visibility: { xs: "hidden", sm: "visible" } }}
-              fontSize="large"
-            />
-            <Button
-              href="/register"
-              className="font-modesto-condensed btn-text"
-              sx={{
-                mt: { xs: -7, sm: 0 },
-                fontSize: { xs: "20px", sm: "40px" },
-              }}
-            >
-              REGISTER
-            </Button>
-          </Box>
-          <img src={BMSLiveSvg} className="rotate live-logo" />
-          <Box sx={{ mt: { xs: 0, sm: 2 } }}>
-            <Typography
-              variant="caption"
-              className="text-texturina"
-              sx={{ fontSize: { xs: 8, sm: 12 } }}
-            >
-              © | Privacy | Terms & Conditions
-            </Typography>
+            <img src={WhiteLogo} className="rotate" width={"20%"} />
+            
+            <Box sx={{flexDirection: {xs: "column", sm: 'row'} }} className="flex align-center">
+              <Button
+                href="/"
+                className="font-modesto-condensed btn-text"
+                sx={{ px: 0, fontSize: { xs: "20px", sm: "40px" }, mt: {xs: -1 ,sm: 0}}}
+
+              >
+                HOME
+              </Button>
+              <Bolt
+                sx={{ px: 0, visibility: {xs: 'hidden', sm: 'visible'} }}
+                fontSize='large'
+              />
+              <Button
+                href="/register"
+                className="font-modesto-condensed btn-text"
+                sx={{ mt: {xs: -7 ,sm: 0}, fontSize: { xs: "20px", sm: "40px" } }}
+              >
+                REGISTER
+              </Button>
+            </Box>
+            <img src={BMSLiveSvg} className="rotate live-logo" />
+            <Box sx={{ mt: { xs: 0, sm: 2 } }}>
+              <Typography
+                variant="caption"
+                className="text-texturina"
+                sx={{ fontSize: { xs: 6, sm: 12 } }}
+              >
+                © | Privacy | Terms & Conditions
+              </Typography>
+            </Box>
           </Box>
         </Box>
-      </Box>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
