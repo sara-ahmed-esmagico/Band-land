92fec1a6fd6259fca8373d1fc01a701b653cd2d8
Vidisha-18 <vidisha.k@esmagico.in>
2023-09-29 19:49:39 +0530

    Changed FAQS

--- src/common/FAQQuestions/question.js
+++ src/common/FAQQuestions/question.js
@@ -3,26 +3,135 @@
 export const questions=[
     {
         pannel:'pannel1',
-        title:"WHAT'S ALLOWED",
-        questions:['Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.'],
+        title:"Is there an all-area-access ticket with an artist meet & greet?",
+        questions:['There are no all-area-access passes.','Perks & privileges on special category tickets are mentioned in the ticket descriptions.'],
         bgColor:'#21A179'
     },
     {
         pannel:'pannel2',
-        title:"WHAT'S ALLOWED",
-        questions:['Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.'],
+        title:"Do you accommodate people with special needs?",
+        questions:['The festival is wheelchair friendly and our crew will be happy to assist anyone with special needs','For more queries, write to us. We encourage attendees to bring their own wheelchairs, one will be provided at the festival only in the case of any on-ground medical emergencies.'],
         bgColor:'#EC2529'
     },
     {
         pannel:'pannel3',
-        title:"CATEGORY HEADER",
-        questions:['Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.'],
+        title:"Is there parking available at the venue?",
+        questions:['We will be sharing more information related to parking closer to the event.'],
         bgColor:'#F6732F'
     },
     {
         pannel:'pannel4',
-        title:"CATEGORY HEADER",
-        questions:['Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.','Your personal belongings- sunglasses, wallets, masks and such.'],
+        title:"Is photography permitted during the show?",
+        questions:['Phone cameras are allowed.','Professional cameras will not be permitted into the venue.'],
         bgColor:'#FFF5E0'
-    }
+    },
+
+    {
+        pannel:'pannel1',
+        title:"Is smoking allowed at the show?",
+        questions:['Smoking is permitted only in designated zones.','Patrons are encouraged to dispose of their cigarette waste responsibly using the bins/pouches provided.'],
+        bgColor:'#21A179'
+    },
+    {
+        pannel:'pannel2',
+        title:"Are handbags and backpacks allowed into the venue?",
+        questions:['Regular sized backpacks or small single compartment bags - like sling bags, handbags, tote bags, etc. are allowed.','Please avoid carrying large backpacks with multiple compartments.','Bags measuring between 12" x 7" & 4" x 2" will be subjected to baggage scanning.','Bags measuring smaller than 10 cm x 5 cm i.e. 4" x 2" will be checked by hand.'],
+        bgColor:'#EC2529'
+    },
+    {
+        pannel:'pannel3',
+        title:"What happens if the event is canceled?",
+        questions:["You'll be notified in advance via email & phone, with an option to claim a refund or carry it forward for a future date (if rescheduled).","Lineups are subject to change in the event of rescheduling."],
+        bgColor:'#F6732F'
+    },
+    {
+        pannel:'pannel4',
+        title:"How do I get a refund if I can't make it?",
+        questions:['Tickets are not transferable or cancellable. In case of any queries, write to us.'],
+        bgColor:'#FFF5E0'
+    },
+
+
+    {
+        pannel:'pannel1',
+        title:"Do I need to be vaccinated to get into the venue?",
+        questions:['In the best interest of fans and staff, we encourage everypne to follow COVID-19 protocols mandated by local governments.','By purchasing tickets to this event, you agree to abide by the health and safety measures in effect at the time of the event, which may include, but not be limited to, wearing masks, providing proof of double vaccination status and/or providing proof of negative COVID-19 test.'],
+        bgColor:'#21A179'
+    },
+    {
+        pannel:'pannel2',
+        title:"Will there be F&B available at the venue?",
+        questions:['Yes, food and beverages will both be available for purchase at the venue.','Outside food or beverages will not be permitted.'],
+        bgColor:'#EC2529'
+    },
+    {
+        pannel:'pannel3',
+        title:"Is the venue air-conditioned?",
+        questions:["No, this is an open air concert festival."],
+        bgColor:'#F6732F'
+    },
+    {
+        pannel:'pannel4',
+        title:"Is this a seated show?",
+        questions:['No, the festival will be a walk-in standing concert experience.','Seating options will be available in the lounges.'],
+        bgColor:'#FFF5E0'
+    },
+
+    {
+        pannel:'pannel1',
+        title:"Will I be able to switch my ticket to a different category?",
+        questions:['No, tickets once purchased will not be upgradable/changeable in terms of category.'],
+        bgColor:'#21A179'
+    },
+    {
+        pannel:'pannel2',
+        title:"How will I get my tickets?",
+        questions:['Tickets will be home delivered at a date closer to the event, updates for the same will be shared across our social media channels.','You can always refer to your digital ticket copy in your Book MyShow transaction history.'],
+        bgColor:'#EC2529'
+    },
+    {
+        pannel:'pannel3',
+        title:"Is there a ticket limit?",
+        questions:["You can buy up to 10 tickets per person. All ticket categories are subject to availability - so get yours before they sell out!"],
+        bgColor:'#F6732F'
+    },
+    {
+        pannel:'pannel4',
+        title:"Will the tickets be available for sale on any other platform?",
+        questions:['No, the official tickets will only be available for sale on BookMyShow. We strongly recommend you not to purchase them via any third party to avoid the possibility of counterfeit tickets.'],
+        bgColor:'#FFF5E0'
+    },
+
+
+
+    {
+        pannel:'pannel1',
+        title:"How can I purchase tickets as an international customer?",
+        questions:['Use the pin/zip code 123456 to digitally make your purchase outside of India. Collect your wristbands at the Box Office on Dec 16-17. For more queries, write here '],
+        bgColor:'#21A179'
+    },
+    {
+        pannel:'pannel2',
+        title:"When is the event taking place?",
+        questions:['Bandland will take place on December 16-17, 2023 at Embassy Riding School, Bengaluru. Festival ground map will be updated closer to the event dates.'],
+        bgColor:'#EC2529'
+    },
+    {
+        pannel:'pannel3',
+        title:"When does the presale go live?",
+        questions:["XXXX, 2023 12:00 PM to xxxx, 2023, 06:00 AM"],
+        bgColor:'#F6732F'
+    },
+    {
+        pannel:'pannel4',
+        title:"When does genera' on-sale begin?",
+        questions:['Tickets go live on xxxx across categories','Ticketing will be active throughout until all tickets are sold out.'],
+        bgColor:'#FFF5E0'
+    },
+    {
+        pannel:'pannel1',
+        title:"How do I contact the festival?",
+        questions:['Follow www.bandland.in / @bandland.in on socials for updates!','bandland.help@bookmyshow.com','Email us on the below links:','General/ticketing queries','Sponsorship inquiries','Press & Editorial inquiries','Corporate bookings','Food vendors '],
+        bgColor:'#21A179'
+    },
 ]
